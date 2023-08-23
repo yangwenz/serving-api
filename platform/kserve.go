857ac9719b880419a5120d08a81b375d41c25f3a
yangwenz <yangwenzhuo08@gmail.com>
2023-08-23 20:00:20 +0800

    First commit

--- platform/kserve.go
+++ platform/kserve.go
@@ -25,60 +25,61 @@ func NewKServe(config utils.Config) Platform {
 	}
 }
 
-func (service *KServe) Predict(
-	request InferRequest,
-	version string,
-) (*InferResponse, *RequestError) {
-	modelName := request.GetModelName()
-	inputs := request.GetInputs()
-
+func (service *KServe) Predict(request InferRequest, version string) (*InferResponse, *RequestError) {
 	if version == "v1" {
-		// Marshal the input data
-		data, err := json.Marshal(inputs)
-		if err != nil {
-			return nil, NewRequestError(MarshalError,
-				errors.New("failed to marshal request"))
-		}
-
-		// Build a new prediction request
-		url := fmt.Sprintf("http://%s/v1/models/%s:predict", service.address, modelName)
-		req, err := http.NewRequest("POST", url, bytes.NewReader(data))
-		if err != nil {
-			return nil, NewRequestError(BuildRequestError,
-				errors.New("failed to build request"))
-		}
-		req.Header.Set("Content-Type", "application/json")
-		req.Host = fmt.Sprintf("%s.%s.%s",
-			modelName, request.GetNamespace(), service.customDomain)
-
-		// Send the prediction request
-		client := http.Client{Timeout: time.Duration(service.timeout) * time.Second}
-		res, err := client.Do(req)
-		if err != nil {
-			return nil, NewRequestError(SendRequestError,
-				errors.New("failed to send request, model not ready"))
-		}
-		if res.StatusCode != 200 {
-			return nil, NewRequestError(InvalidInputError,
-				errors.New("invalid model or inputs"))
-		}
-
-		// Parse the response
-		defer res.Body.Close()
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			return nil, NewRequestError(ReadResponseError,
-				errors.New("failed to read response body"))
-		}
-		var outputs map[string]interface{}
-		err = json.Unmarshal(body, &outputs)
-		if err != nil {
-			return nil, NewRequestError(UnmarshalResponseError,
-				errors.New("failed to unmarshal response body"))
-		}
-		response := InferResponse{Outputs: outputs}
-		return &response, nil
+		return service.predictV1(request, version)
 	}
 	return nil, NewRequestError(UnknownAPIVersion,
 		errors.New("prediction API version is not supported"))
+}
+
+func (service *KServe) predictV1(request InferRequest, version string) (*InferResponse, *RequestError) {
+	modelName := request.GetModelName()
+	inputs := request.GetInputs()
+
+	// Marshal the input data
+	data, err := json.Marshal(inputs)
+	if err != nil {
+		return nil, NewRequestError(MarshalError,
+			errors.New("failed to marshal request"))
+	}
+
+	// Build a new prediction request
+	url := fmt.Sprintf("http://%s/v1/models/%s:predict", service.address, modelName)
+	req, err := http.NewRequest("POST", url, bytes.NewReader(data))
+	if err != nil {
+		return nil, NewRequestError(BuildRequestError,
+			errors.New("failed to build request"))
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Host = fmt.Sprintf("%s.%s.%s",
+		modelName, request.GetNamespace(), service.customDomain)
+
+	// Send the prediction request
+	client := http.Client{Timeout: time.Duration(service.timeout) * time.Second}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, NewRequestError(SendRequestError,
+			errors.New("failed to send request, model not ready"))
+	}
+	if res.StatusCode != 200 {
+		return nil, NewRequestError(InvalidInputError,
+			errors.New("invalid model or inputs"))
+	}
+
+	// Parse the response
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, NewRequestError(ReadResponseError,
+			errors.New("failed to read response body"))
+	}
+	var outputs map[string]interface{}
+	err = json.Unmarshal(body, &outputs)
+	if err != nil {
+		return nil, NewRequestError(UnmarshalResponseError,
+			errors.New("failed to unmarshal response body"))
+	}
+	response := InferResponse{Outputs: outputs}
+	return &response, nil
 }
